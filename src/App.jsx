926a58c987ffe8014034d128e6734f5ecdd9c256
qgillia <qgillia@gmail.com>
2024-08-18 01:12:56 -0400

    add Sentry

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
 
+import * as Sentry from '@sentry/react';
+
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
-      <Model/>
+      <Model />
     </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
